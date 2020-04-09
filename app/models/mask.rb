ed9be0d9da227362a7c9afe3c6ecc827e2475b71
Stephanie Dinu <stephaniedinu@gmail.com>
2020-04-09 16:13:35 +0200

    model validations complete

--- app/models/mask.rb
+++ app/models/mask.rb
@@ -1,6 +1,8 @@
 class Mask < ApplicationRecord
   belongs_to :user
-
   has_many :reservations
   has_many :reviews, as: :reviewable
+  validates :description, presence: true
+  validates :condition, presence: true
+  validates :size, presence: true, inclusion: { in: %w(Child Adult Hagrid) }
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :mask
   belongs_to :user
-
   has_many :reviews, as: :reviewable
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :active, default: true
 end
