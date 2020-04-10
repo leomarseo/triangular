250b811e49f20872d291d2c794830ac2e459932b
Leonardo Marsigli <57939441+leomarseo@users.noreply.github.com>
2020-04-10 17:21:17 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#index'
   get 'pages/dashboard', to: 'pages#dashboard'
+  get 'pages/home', to: 'pages#home'
   resources :masks, only: %i[index new create edit show update] do
     resources :reservations, only: %i[new create edit show update]
   end
