72f6c3e79d0e2a934961ac3f4f3b454a4d57ba3e
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-10 17:22:27 +0200

    added pages-dashboard with relevant masks controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
-  get '/dashboard', to: 'pages#dashboard'
+  get 'pages/dashboard', to: 'pages#dashboard'
   resources :masks, only: %i[index new create edit show update] do
     resources :reservations, only: %i[new create edit show update]
   end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get dashboard" do
+    get pages_dashboard_url
+    assert_response :success
+  end
+
+end
