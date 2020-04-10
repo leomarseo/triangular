ad56f8ecade9866ac3ba7bdd10430cbe1e9644c5
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-10 18:30:21 +0200

    fixed schema active-deleted, added name to masks and seed

--- db/migrate/20200409170148_remove_active_from_masks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveActiveFromMasks < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :masks, :active, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_170148) do
+ActiveRecord::Schema.define(version: 2020_04_10_161905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_170148) do
     t.date "start_time"
     t.date "end_time"
     t.float "price"
+    t.string "name"
     t.index ["user_id"], name: "index_masks_on_user_id"
   end
 
@@ -36,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_170148) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "active"
+    t.boolean "deleted"
     t.index ["mask_id"], name: "index_reservations_on_mask_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
