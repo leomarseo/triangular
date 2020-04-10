17d4925fa40e07ef19f32de3015297294f22ec32
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-10 18:51:00 +0200

    finally fixed schema and migrations

--- db/migrate/20200410163118_add_address.rb
+++
@@ -1,5 +0,0 @@
-class AddAddress < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :address, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2020_04_10_161905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "listings", force: :cascade do |t|
-    t.float "price"
-    t.string "start_time"
-    t.string "end_time"
-    t.bigint "mask_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["mask_id"], name: "index_listings_on_mask_id"
-  end
-
   create_table "masks", force: :cascade do |t|
     t.string "description"
     t.string "condition"
@@ -41,8 +31,8 @@ ActiveRecord::Schema.define(version: 2020_04_10_161905) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.string "start_time"
-    t.string "end_time"
+    t.date "start_time"
+    t.date "end_time"
     t.bigint "mask_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -63,19 +53,18 @@ ActiveRecord::Schema.define(version: 2020_04_10_161905) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "listings", "masks"
   add_foreign_key "masks", "users"
   add_foreign_key "reservations", "masks"
   add_foreign_key "reservations", "users"
