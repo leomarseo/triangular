7cfd46c0cb6a4f5d6d20621da713ebeaeee4bccd
Gian Marco Romani <gromani92@gmail.com>
2020-04-10 16:38:37 +0200

    Front-end: added a landing page and change some file location

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "listings#index"
+  root to: "pages#home"
   resources :listings do
     resources :reservations, only: %i(new create edit show update)
   end
