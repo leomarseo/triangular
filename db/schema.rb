866bdb7f422ecdc19c824439ebdfc0c900983d30
Stephanie Dinu <stephaniedinu@gmail.com>
2020-04-10 19:28:09 +0200

    committing changes made by merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_161905) do
+ActiveRecord::Schema.define(version: 2020_04_10_163118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.float "price"
+    t.string "start_time"
+    t.string "end_time"
+    t.bigint "mask_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mask_id"], name: "index_listings_on_mask_id"
+  end
 
   create_table "masks", force: :cascade do |t|
     t.string "description"
@@ -31,13 +41,13 @@ ActiveRecord::Schema.define(version: 2020_04_10_161905) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.date "start_time"
-    t.date "end_time"
+    t.string "start_time"
+    t.string "end_time"
     t.bigint "mask_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "deleted"
+    t.boolean "active"
     t.index ["mask_id"], name: "index_reservations_on_mask_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
@@ -53,18 +63,19 @@ ActiveRecord::Schema.define(version: 2020_04_10_161905) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "listings", "masks"
   add_foreign_key "masks", "users"
   add_foreign_key "reservations", "masks"
   add_foreign_key "reservations", "users"
