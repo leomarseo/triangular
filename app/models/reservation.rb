b036af52eea44e78d543e25be90ef1e5ca2ff13b
Kevin Kim <khk271@nyu.edu>
2020-04-12 20:18:10 -0400

    completed index, show, new, and create controller methods and views for reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Reservation < ApplicationRecord
   belongs_to :mask
   belongs_to :user
@@ -5,4 +7,15 @@ class Reservation < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :active, default: true
+
+  def reservation_status(start_date, end_date)
+    date_today = DateTime.current.beginning_of_day
+    if date_today > end_date
+      'expired'
+    elsif (date_today <= end_date) && (date_today >= start_date)
+      'active'
+    else
+      'inactive'
+    end
+  end
 end
