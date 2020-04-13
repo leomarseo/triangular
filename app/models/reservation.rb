065a8f5106930d9e16d4a76312e23d391434d313
Kevin Kim <khk271@nyu.edu>
2020-04-13 08:27:54 -0400

    completed basic pages, still requires validations for reservation actions

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,6 @@ class Reservation < ApplicationRecord
   has_many :reviews, as: :reviewable
   validates :start_time, presence: true
   validates :end_time, presence: true
-  validates :active, default: true
 
   def reservation_status(start_date, end_date)
     date_today = DateTime.current.beginning_of_day
