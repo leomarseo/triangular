5e7656bc2aa00758e7d99dc8e6e1ca445ede201f
Fernando Lazzarin <fernandolazzarin@yahoo.com.br>
2020-04-14 18:08:19 +0200

    added javascript

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.geocoded # returns users with coordinates
+
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,11 @@ require("channels")
 import 'bootstrap';
 import '../../assets/stylesheets/application';
 
+import { initMapbox } from '../plugins/init_mapbox';
 
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
