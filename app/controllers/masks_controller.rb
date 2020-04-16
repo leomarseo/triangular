498684011b95a892843b7bbe187d0fd5232d26cc
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-16 13:19:26 +0200

    fixed search bar

--- app/controllers/masks_controller.rb
+++ app/controllers/masks_controller.rb
@@ -4,9 +4,17 @@ class MasksController < ApplicationController
     @masks = Mask.all
     return if real_parameters.nil?
 
-    @location = real_parameters[:location]
-    @start_time = Date.parse(real_parameters[:start_time]) unless real_parameters[:start_time].nil? || real_parameters[:start_time] == ""
-    @end_time = Date.parse(real_parameters[:end_time]) unless real_parameters[:end_time].nil? || real_parameters[:end_time] == ""
+    @location = real_parameters[:location].downcase.capitalize unless real_parameters[:location].nil? || real_parameters[:location] == ""
+
+    if !real_parameters[:start_time].nil? && real_parameters[:start_time] != ""
+      @full_time = real_parameters[:start_time]
+      @start_time = Date.parse(@full_time.chars.first(10).join)
+      @end_time = Date.parse(@full_time.chars.last(10).join)
+    elsif !@range_time.nil?
+      @start_time = Date.parse(@range_time.chars.first(10).join)
+      @end_time = Date.parse(@range_time.chars.last(10).join)
+    end
+
     @size = real_parameters[:size] unless real_parameters[:size].nil? || real_parameters[:size] == ""
 
     if real_parameters[:start_time].present? && real_parameters[:location].present? && real_parameters[:size].present?
@@ -27,17 +35,9 @@ class MasksController < ApplicationController
     elsif real_parameters[:start_time].present?
       @masks = Mask.where(['start_time < ? AND end_time > ?', @start_time, @end_time])
 
-      # @masks_dates = @masks.where(['start_time < ? AND end_time > ?', parsed_time_start, parsed_time_end])
     elsif real_parameters[:location].present?
       @masks = Mask.joins(:user).where('users.address like ?', "%#{@location}%")
 
-    # elsif real_parameters[:size].present? || real_parameters[:condition].present? || real_parameters[:price].present?
-    #   sql_query = "
-    #    CAST(size AS text) @@ :query \
-    #    OR CAST(condition AS text) @@ :query \
-    #    OR CAST(price AS text) ILIKE :query \
-    #   "
-    #   @masks = Mask.where(sql_query, query: "%#{real_parameters[:size]}%").where(sql_query, query: "%#{real_parameters[:condition]}%").where(sql_query, query: "#{real_parameters[:price]}")
     else
       @masks = Mask.all
     end
